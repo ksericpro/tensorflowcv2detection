ed9a0cfcb42dfa3f4db9a7832e93f3fdd7258e0f
ksericpro <ksericpro@gmail.com>
2023-12-06 12:15:58 +0800

    detection.ipynb

--- config/local.py
+++ config/local.py
@@ -1,5 +1,6 @@
 label_file = 'labels.csv'
 width = 1028
 height = 1028
-model_uri = "https://www.kaggle.com/models/tensorflow/efficientdet/frameworks/TensorFlow2/variations/d0/versions/1"
+#model_uri = "https://www.kaggle.com/models/tensorflow/efficientdet/frameworks/TensorFlow2/variations/d0/versions/1"
+model_uri = "./models/efficientdet/d0/1"
 image = "image_1.jpg"
